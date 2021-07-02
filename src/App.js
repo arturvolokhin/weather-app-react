2b8e6519526fdb8c7bd170275f84ddefaf8584cd
Arthur Volokhin <artur.volokhin@mail.ru>
2021-07-02 11:16:51 +0300

    feat: Component TodayData

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Search from './Components/Search'
+import TodayData from './Components/TodayData';
 import './main.css';
 
 function App() {
     return (
         <>
             <Search/>
+            <main className='main'>
+                <TodayData/>
+            </main>
         </>
     );
 }
---
+++ src/components/TodayData.js
@@ -0,0 +1,16 @@
+const TodayData = () => {
+    return(
+        <>
+            <div className="main__header">
+                <div className="main__weather">
+                    <h2 className="main__temperature"></h2>
+                    <img className="main__icon"/>
+                </div>
+                <h2 className="main__location"></h2>
+                <p className="main__today"></p>
+            </div>
+        </>
+    )
+}
+
+export default TodayData;
